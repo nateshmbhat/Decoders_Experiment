bf3b00013ae4c7850505184a8ea79afc7fc7e5c0
nateshmbhat <nateshmbhat1@gmail.com>
2017-10-20 13:15:33 +0530

    fixed some indentation

--- landing/carouselText.inc.php
+++ landing/carouselText.inc.php
@@ -2,6 +2,6 @@
 	<div class="col-md-12"><h1 class="h1-responsive">Welcome to the website of<strong>DeCoders</strong></h1></div>
 		<p>Where coding begins all over again</p>
 			<button type="button" class="btn btn-primary btn-lg"  data-toggle="modal" data-target="#modalForm">
-					Natesh Login
+					Member Login
 			</button>
 </div>
